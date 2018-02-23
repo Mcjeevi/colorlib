d4e69b246c37b16adfb090f1d06efa82cf47b9e9
unknown <cjeevi.muvva@gmail.com>
2018-02-23 17:42:19 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    if($('nav.navbar').css('display','block')){
+        $('nav.navbar').css('display','none');
+    }else{
+        $('nav.navbar').css('display','block');
+    }
+}
